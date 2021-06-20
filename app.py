7591da35fd0da4e7e7dd476e291e85bab31a7c3c
FabienCruz <cruzfabien@gmail.com>
2021-06-20 19:17:44 +0200

    delete finished and start update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import pathlib, glob, re, markdown, datetime
+import markdown, datetime
 from bson.objectid import ObjectId
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, RadioField, DateTimeField
 #from wtforms.fields.core import RadioField
@@ -27,6 +27,15 @@ class Form(FlaskForm):
     date = DateTimeField('date')
     draft = RadioField('draft', choices=[('True', 'brouillon'), ('False', 'publication')], default='True')
     text = TextAreaField('texte')
+
+#--------------------------
+# Utilities
+#--------------------------
+def get_files(draft):
+    files = []
+    for file in sheets.find({'draft': draft}, {'title': 1}):
+        files.append(file)
+    return files
 
 #--------------------------
 # Routes
@@ -56,7 +65,18 @@ def new_submission():
     new_document['date'] = datetime.datetime.now()
     new_document['content'] = form.text.data
     sheets.insert_one(new_document)
+    flash ('the document is created')
     return redirect(url_for('index'))
+
+@app.route('/sheet/update/<id>', methods=['GET'])
+def update(id):
+    form = Form()
+    sheet = sheets.find_one({"_id": ObjectId(id)})
+    form.title.data = sheet['title']
+    form.draft.data = sheet['draft']
+    form.text.data = sheet['content']
+    return render_template('form.html', form=form)
+
 
 @app.route('/sheet/<id>', methods=['DELETE'])
 def delete(id):
@@ -68,9 +88,7 @@ def delete(id):
 
 @app.route('/sheets/<draft>', methods=['GET', 'DELETE'])
 def show_list(draft):
-    files = []
-    for file in sheets.find({'draft': draft}, {'title': 1}):
-        files.append(file)
+    files = get_files(draft)
     return render_template('list.html', files=files)
 
 if __name__ == '__main__':
@@ -84,17 +102,15 @@ if __name__ == '__main__':
 
 --create a mini-cms with flask WTF
 
-pick-up file in a list
-read file 
-create file
-differenciate draft and publish
-show only publish
-modify file
+x pick-up file in a list
+x read file 
+x create file
+x differenciate draft and publish
+x show only publish
 delete file
+modify file
 
 --create access and authorization
 
 --deploy
-
---create another version with a postgresql database / mongodb
 '''
